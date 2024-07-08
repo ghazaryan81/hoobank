e0d17f73cdf3ead508d2a295249f027be970eb7b
Hayk Ghazaryan <hayk.ghazaryanc@gmail.com>
2024-07-08 22:20:06 +0400

    set the colors & gradients

--- src/app/design-tokens/colors.ts
+++ src/app/design-tokens/colors.ts
@@ -2,29 +2,32 @@ export const colors = {
   "black": "rgba( 0 0 0 / <alpha-value> )",
   "white": "rgba( 255 255 255 / <alpha-value> )",
 
-  "green": {
-    "500": "rgba( 0 174 28 / <alpha-value> )"
-  },
-
-  "grey": {
-    "400": "rgba( 63 62 69 / <alpha-value> )",
-    "500": "rgba( 57 57 57 / <alpha-value> )"
+  "gray": {
+    "500": "rgba( 63 62 69 / <alpha-value> )",
+    "600": "rgba( 57 57 57 / <alpha-value> )",
+    "700": "rgba( 63 62 69 / <alpha-value> )",
+    "750": "rgba( 6 8 13 / <alpha-value> )",
+    "800": "rgba( 0 4 14 / <alpha-value> )"
   },
 
   "aqua": {
-    "50": "rgba(224 249 250   / <alpha-value>)",
-    "100": "rgba(190 243 245  / <alpha-value>)",
-    "200": "rgba(157 237 240  / <alpha-value>)",
-    "300": "rgba(125 231 235  / <alpha-value>)",
-    "400": "rgba(92 225 230   / <alpha-value>)",
-    "500": "rgba(0 246 255    / <alpha-value>)",
-    "600": "rgba(92 225 230   / <alpha-value>)",
-    "700": "rgba(51 187 207   / <alpha-value>)",
-    "800": "rgba(38 208 206   / <alpha-value>)",
-    "900": "rgba(2 20 27      / <alpha-value>)"
+    "50": "rgba( 222 249 250 / <alpha-value>)",
+    "100": "rgba( 190 243 245 / <alpha-value>)",
+    "200": "rgba( 157 237 240 / <alpha-value>)",
+    "300": "rgba( 125 231 235 / <alpha-value>)",
+    "400": "rgba( 92 225 230  / <alpha-value>)",
+    "500": "rgba( 0 246 255   / <alpha-value>)",
+    "600": "rgba( 92 225 230  / <alpha-value>)",
+    "700": "rgba( 51 187 207  / <alpha-value>)",
+    "800": "rgba( 38 208 206  / <alpha-value>)",
+    "900": "rgba( 2 20 27     / <alpha-value>)"
   },
 
-  "dark-blue": {
-    "500": "rgba( 0 4 15 / <alpha-value> )"
+  "blue": {
+    "500": "rgba( 26 41 128 / <alpha-value> )"
+  },
+
+  "violet": {
+    "500": "rgba( 252 103 250 / <alpha-value> )"
   }
 };
