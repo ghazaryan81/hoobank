445d974b2f7a4205dcbd5d8b5f8697688bd4da30
Hayk Ghazaryan <hayk.ghazaryanc@gmail.com>
2024-07-19 13:30:23 +0400

    feat: billing-control section is done

--- src/app/design-tokens/width.ts
+++ src/app/design-tokens/width.ts
@@ -1,13 +1,12 @@
 /* prettier-ignore */ import { layoutSizes } from "../def/base/layout-sizes";
-/* prettier-ignore */ import { toRemObj }    from "../../app/func";
+/* prettier-ignore */ import { toRemObj } from "../../app/func";
 
 /* prettier-ignore */
-export const width = Object.assign(
-	toRemObj(layoutSizes.fullWidths()), {
-  "50p": "50%",
-  "60p": "60%",
-  "70p": "70%",
-  "80p": "80%",
-  "90p": "90%",
-  "100p": "100%",
+export const width = Object.assign(toRemObj(layoutSizes.fullWidths()), {
+  "50p":  "50%",
+  "60p":  "60%",
+  "70p":  "70%",
+  "80p":  "80%",
+  "90p":  "90%",
+  "100p": "100%"
 });
